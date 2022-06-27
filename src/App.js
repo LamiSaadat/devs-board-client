434c6cf4867c35db1622f7d049819878ad132766
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-26 23:12:26 -0400

    Generate color data from server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
+import ColorPalette from "./components/ColorPalette/ColorPalette";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ColorPalette />
     </div>
   );
 }
--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -1,7 +1,25 @@
 import "./ColorPalette.scss";
+import React, { useState } from "react";
+import axios from "axios";
 
 const ColorPalette = () => {
-  return <div>Color Palette</div>;
+  const [colorPalette, setColorPalette] = useState(null);
+
+  const updateColorPalette = () => {
+    axios.get("http://localhost:8080/palettes").then((response) => {
+      setColorPalette(response.data);
+      console.log(response.data);
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <div>{colorPalette}</div>
+        <button onClick={updateColorPalette}>Click me</button>
+      </div>
+    </>
+  );
 };
 
 export default ColorPalette;
