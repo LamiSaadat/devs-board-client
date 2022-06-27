2c7db7e7bc7ebca0608fa2d37ec0ce7ab39a2b85
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-27 15:38:14 -0400

    Generate color palette from back end and display on front end

--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -1,22 +1,41 @@
 import "./ColorPalette.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 const ColorPalette = () => {
-  const [colorPalette, setColorPalette] = useState(null);
+  const [colorPalette, setColorPalette] = useState([]);
+  // const [keyword, setKeyword] = useState("");
 
-  const updateColorPalette = () => {
+  useEffect(() => {
     axios.get("http://localhost:8080/palettes").then((response) => {
       setColorPalette(response.data);
       console.log(response.data);
     });
-  };
+  }, []);
+
+  const ColorCard = styled.div`
+    width: 2rem;
+    height: 2rem;
+    margin-bottom: 2rem;
+  `;
 
   return (
     <>
       <div>
-        <div>{colorPalette}</div>
-        <button onClick={updateColorPalette}>Click me</button>
+        <input type="text" className="keyword-input"></input>
+        {colorPalette.map((color) => {
+          return (
+            <ColorCard
+              key={color.color.clean}
+              style={{
+                backgroundColor: `${color.color.value}`,
+              }}
+            ></ColorCard>
+          );
+        })}
+
+        <button>Click me</button>
       </div>
     </>
   );
