bab549a2257d5417fb625ace4f5e30a704091c4b
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-28 07:35:03 -0400

    Add bootstrap header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,30 @@
 import "./Header.scss";
 
 function Header() {
-  return <div>I am the Header</div>;
+  return (
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a
+        href="/"
+        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+      >
+        <img class="bi me-2" width="40" height="32"></img>
+        <span class="fs-4">Dev's Board</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            Boards
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
