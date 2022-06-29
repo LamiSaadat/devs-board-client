484e3655123792a7223ca14ea715100c3ecad3e7
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-29 10:08:21 -0400

    Make post request to create new board entry in board table in database

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 import ColorPalette from "../ColorPalette/ColorPalette";
 import "./Board.scss";
 
 function Board() {
   const location = useLocation();
   const { boardName, keyword } = location.state;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newBoard = { name: boardName };
+
+    axios.post("http://localhost:8080/boards", newBoard).then((response) => {
+      console.log(response);
+    });
+  };
   return (
     <>
-      <p>{boardName}</p>
-      <ColorPalette keyword={keyword} />
+      <form onSubmit={handleSubmit}>
+        <p>{boardName}</p>
+        <ColorPalette keyword={keyword} />
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 }
