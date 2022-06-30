b469b7edda45bcf5b1cff35798e78a684585a6fa
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-30 16:24:39 -0400

    Display all boards in boards gallery

--- src/components/BoardsGallery/BoardsGallery.js
+++ src/components/BoardsGallery/BoardsGallery.js
@@ -1,7 +1,26 @@
 import React from "react";
+import axios from "axios";
 
-function BoardsGallery() {
-  return <div>BoardsGallery</div>;
+function BoardsGallery({ boards }) {
+  // const allBoards = () => {
+  //   axios
+  //     .get("http://localhost:8080/boards")
+  //     .then((response) => console.log(response.data));
+  // };
+
+  // let getBoard = allBoards();
+
+  return (
+    <div>
+      {boards.map((board) => {
+        return (
+          <div>
+            <p>{board.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default BoardsGallery;
--- src/pages/BoardPage/BoardPage.js
+++ src/pages/BoardPage/BoardPage.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 import "./BoardPage.scss";
 import Board from "../../components/Board";
 import BoardCreator from "../../components/BoardCreator";
 import BoardsGallery from "../../components/BoardsGallery";
 
 function BoardPage() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/boards").then((response) => {
+      console.log(response.data);
+      setBoards(response.data);
+    });
+  }, []);
+
   return (
     <Router>
       <Switch>
         <Route path="/board/create" component={BoardCreator}></Route>
-        <Route path="/board/gallery" component={BoardsGallery}></Route>
-        <Route path="/board/:id" component={Board}></Route>
+        <Route
+          path="/board/gallery"
+          render={(routerProps) => {
+            return <BoardsGallery boards={boards} {...routerProps} />;
+          }}
+        ></Route>
+        <Route
+          path="/board/:id"
+          render={(routerProps) => {
+            return <Board boards={boards} {...routerProps} />;
+          }}
+        ></Route>
       </Switch>
     </Router>
   );
