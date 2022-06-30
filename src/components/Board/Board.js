0e8227fdf09806b30a2af92146e3811c11cbc473
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-06-29 22:05:07 -0400

    Created post requests to send board data to backend with dummy id for palette

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -20,10 +20,26 @@ function Board() {
     e.preventDefault();
 
     let newBoard = { name: boardName };
+    let newColorPalette = {
+      color1: colorPalette[0].color.value,
+      color2: colorPalette[1].color.value,
+      color3: colorPalette[2].color.value,
+      color4: colorPalette[3].color.value,
+      color5: colorPalette[4].color.value,
+      board_id: 1,
+    };
+
+    console.log(newColorPalette);
 
     axios.post("http://localhost:8080/boards", newBoard).then((response) => {
       console.log(response.data);
     });
+
+    axios
+      .post(`http://localhost:8080/palettes/${keyword}`, newColorPalette)
+      .then((response) => {
+        console.log(response.data);
+      });
   };
   return (
     <>
