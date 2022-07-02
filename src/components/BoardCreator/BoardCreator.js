29f8eeb39e97c53a4818bcb2cdf9aced0fe17805
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-02 14:54:50 -0400

    Add functionality to board creator to make API calls to generate images. Update images component to render images

--- src/components/BoardCreator/BoardCreator.js
+++ src/components/BoardCreator/BoardCreator.js
@@ -3,18 +3,35 @@ import { useLocation, useHistory } from "react-router-dom";
 import axios from "axios";
 import ColorPalette from "../ColorPalette/ColorPalette";
 import "./BoardCreator.scss";
+import Images from "../Images/Images";
 
 function BoardCreator() {
   const location = useLocation();
   const history = useHistory();
   const { boardName, keyword } = location.state;
   const [colorPalette, setColorPalette] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:8080/palettes/${keyword}`).then((response) => {
       setColorPalette(response.data);
       console.log(response.data);
     });
+  }, []);
+
+  useEffect(() => {
+    const url = `https://api.pexels.com/v1/search?query=${keyword}&per_page=10`;
+    const API_key = "563492ad6f9170000100000157684eda2eba41cc9560cbd8993465b8";
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `${API_key}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.photos);
+        setImages(response.data.photos);
+      });
   }, []);
 
   const handleSubmit = (e) => {
@@ -51,6 +68,7 @@ function BoardCreator() {
       <form onSubmit={handleSubmit}>
         <p>{boardName}</p>
         <ColorPalette colorPalette={colorPalette} />
+        <Images images={images}></Images>
         <button type="submit">Save</button>
       </form>
     </>
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Images() {
-  return <div>Images</div>;
+function Images({ images }) {
+  return (
+    <div>
+      {images.map((image) => {
+        return <img src={image.src.small} />;
+      })}
+    </div>
+  );
 }
 
 export default Images;
