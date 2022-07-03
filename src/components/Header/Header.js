cdfad47b0a86bd990ee01e6258e6db2efa7ecb74
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-02 21:52:43 -0400

    Moved partials folder out of src and into a new styles folder. Added styles to header and corresponding classes to header jsx

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,23 +2,27 @@ import "./Header.scss";
 
 function Header() {
   return (
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom header">
       <a
         href="/"
-        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
       >
-        <img class="bi me-2" width="40" height="32"></img>
-        <span class="fs-4">Dev's Board</span>
+        {/* <img className="bi me-2 header__logo" width="40" height="32"></img> */}
+        <span className="fs-4 header__title">Dev's Board</span>
       </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">
+      <ul className="nav nav-pills">
+        <li className="nav-item header__link header__item--active">
+          <a
+            href="#"
+            className="nav-link header__item-link"
+            aria-current="page"
+          >
             Home
           </a>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+        <li className="nav-item header__item">
+          <a href="#" className="nav-link header__item-link">
             Boards
           </a>
         </li>
