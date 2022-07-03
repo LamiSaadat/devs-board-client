624291cfd0e8a5df44a2b962553193c05f7fe2ec
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-03 12:58:23 -0400

    Add styles to board gallery and corresponding class names and jsx

--- src/components/BoardsGallery/BoardsGallery.js
+++ src/components/BoardsGallery/BoardsGallery.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./BoardsGallery.scss";
 
 function BoardsGallery({ boards }) {
   return (
-    <div>
-      {boards.map((board) => {
-        return (
-          <Link to={`/board/${board.id}`}>
-            <p>{board.name}</p>
-          </Link>
-        );
-      })}
-    </div>
+    <section className="board-gallery-container">
+      <h1 className="board-gallery__title">My Boards</h1>
+      <div className="board-gallery">
+        {boards.map((board) => {
+          return (
+            <Link to={`/board/${board.id}`} className="board">
+              <p className="board__name">{board.name}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 }
 
