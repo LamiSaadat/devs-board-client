38d73e8a483f26b7f915bcc01a14ccfbdf24cca8
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-02 22:26:20 -0400

    Updated form styles. Removed homepage texts from form component

--- src/components/CreateBoardForm/CreateBoardForm.js
+++ src/components/CreateBoardForm/CreateBoardForm.js
@@ -24,48 +24,43 @@ function CreateBoardForm() {
   };
 
   return (
-    <div className="container col-xl-10 col-xxl-8 px-4 py-5">
-      <div className="row align-items-center g-lg-5 py-5">
-        <div className="col-lg-7 text-center text-lg-start">
-          <h1 className="display-4 fw-bold lh-1 mb-3">Dev's Board</h1>
-          <p className="col-lg-10 fs-4">For devs by a dev :D</p>
+    <div className="col-md-10 mx-auto col-lg-5 form-container">
+      <form
+        className="p-4 p-md-5 border rounded-3 bg-light form"
+        onSubmit={handleSubmit}
+      >
+        <div className="form-floating mb-3 form__input-container">
+          <input
+            type="text"
+            className="form-control form__input"
+            id="floatingInput"
+            name="boardName"
+            value={boardName}
+            onChange={handleBoardNameChange}
+            placeholder="Enter board name..."
+          />
+          <label for="floatingInput">Board Name</label>
         </div>
-        <div className="col-md-10 mx-auto col-lg-5">
-          <form
-            className="p-4 p-md-5 border rounded-3 bg-light"
-            onSubmit={handleSubmit}
-          >
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="floatingInput"
-                name="boardName"
-                value={boardName}
-                onChange={handleBoardNameChange}
-                placeholder="Enter board name..."
-              />
-              <label for="floatingInput">Board Name</label>
-            </div>
-            <div className="form-floating mb-3">
-              <input
-                type="text"
-                className="form-control"
-                id="floatingPassword"
-                name="keyword"
-                value={keyword}
-                onChange={handleKeywordChange}
-                placeholder="Enter keyword..."
-              />
-              <label for="floatingPassword">Keyword</label>
-            </div>
+        <div className="form-floating mb-3 form__input-container">
+          <input
+            type="text"
+            className="form-control form__input"
+            id="floatingPassword"
+            name="keyword"
+            value={keyword}
+            onChange={handleKeywordChange}
+            placeholder="Enter keyword..."
+          />
+          <label for="floatingPassword">Keyword</label>
+        </div>
 
-            <button className="w-100 btn btn-lg btn-primary" type="submit">
-              Create Board
-            </button>
-          </form>
-        </div>
-      </div>
+        <button
+          className="w-100 btn btn-lg btn-primary form__btn"
+          type="submit"
+        >
+          Create Board
+        </button>
+      </form>
     </div>
   );
 }
