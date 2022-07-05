87aaf50cd82d9075168b7f0fdcd3379b1805361d
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-05 13:46:42 -0400

    Add overlay to color swatches to show color hex codes

--- src/components/BoardColorPalette/BoardColorPalette.js
+++ src/components/BoardColorPalette/BoardColorPalette.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import copy3 from "../../assets/icons/copy-icon-3.png";
 
 function BoardColorPalette({ boardColorPalette, onTileClick }) {
   const ColorCard = styled.div`
@@ -10,86 +9,78 @@ function BoardColorPalette({ boardColorPalette, onTileClick }) {
   return (
     <div>
       <div className="palette">
-        <div className="palette__card">
-          <ColorCard
-            style={{
-              backgroundColor: `${boardColorPalette[0].color1}`,
-            }}
-            onClick={() => onTileClick(boardColorPalette[0].color1)}
-            className="palette__swatch"
-          />
+        <ColorCard
+          style={{
+            backgroundColor: `${boardColorPalette[0].color1}`,
+          }}
+          onClick={() => onTileClick(boardColorPalette[0].color1)}
+          className="palette__swatch"
+        >
           <div
             className="palette__info"
             onClick={() => onTileClick(boardColorPalette[0].color1)}
           >
-            <span>{boardColorPalette[0].color1}</span>
-            <img className="palette__copy" src={copy3}></img>
+            {boardColorPalette[0].color1}
           </div>
-        </div>
-        <div className="palette__card">
-          <ColorCard
-            style={{
-              backgroundColor: `${boardColorPalette[0].color2}`,
-            }}
-            onClick={() => onTileClick(boardColorPalette[0].color2)}
-            className="palette__swatch"
-          />
+        </ColorCard>
+        <ColorCard
+          style={{
+            backgroundColor: `${boardColorPalette[0].color2}`,
+          }}
+          onClick={() => onTileClick(boardColorPalette[0].color2)}
+          className="palette__swatch"
+        >
           <div
             className="palette__info"
             onClick={() => onTileClick(boardColorPalette[0].color2)}
           >
-            <span>{boardColorPalette[0].color2}</span>
-            <img className="palette__copy" src={copy3}></img>
+            {boardColorPalette[0].color2}
           </div>
-        </div>
-        <div className="palette__card">
-          <ColorCard
-            style={{
-              backgroundColor: `${boardColorPalette[0].color3}`,
-            }}
-            onClick={() => onTileClick(boardColorPalette[0].color3)}
-            className="palette__swatch"
-          />
+        </ColorCard>
+
+        <ColorCard
+          style={{
+            backgroundColor: `${boardColorPalette[0].color3}`,
+          }}
+          onClick={() => onTileClick(boardColorPalette[0].color3)}
+          className="palette__swatch"
+        >
           <div
             className="palette__info"
             onClick={() => onTileClick(boardColorPalette[0].color3)}
           >
-            <span>{boardColorPalette[0].color3}</span>
-            <img className="palette__copy" src={copy3}></img>
+            {boardColorPalette[0].color3}
           </div>
-        </div>
-        <div className="palette__card">
-          <ColorCard
-            style={{
-              backgroundColor: `${boardColorPalette[0].color4}`,
-            }}
-            onClick={() => onTileClick(boardColorPalette[0].color4)}
-            className="palette__swatch"
-          />
+        </ColorCard>
+
+        <ColorCard
+          style={{
+            backgroundColor: `${boardColorPalette[0].color4}`,
+          }}
+          onClick={() => onTileClick(boardColorPalette[0].color4)}
+          className="palette__swatch"
+        >
           <div
             className="palette__info"
             onClick={() => onTileClick(boardColorPalette[0].color4)}
           >
-            <span>{boardColorPalette[0].color4}</span>
-            <img className="palette__copy" src={copy3}></img>
+            {boardColorPalette[0].color4}
           </div>
-        </div>
-        <div className="palette__card">
-          <ColorCard
-            style={{
-              backgroundColor: `${boardColorPalette[0].color5}`,
-            }}
-            onClick={() => onTileClick(boardColorPalette[0].color5)}
-            className="palette__swatch"
-          />
+        </ColorCard>
+        <ColorCard
+          style={{
+            backgroundColor: `${boardColorPalette[0].color5}`,
+          }}
+          onClick={() => onTileClick(boardColorPalette[0].color5)}
+          className="palette__swatch"
+        >
           <div
             className="palette__info"
             onClick={() => onTileClick(boardColorPalette[0].color5)}
           >
-            <span>{boardColorPalette[0].color5}</span>
-            <img className="palette__copy" src={copy3}></img>
+            {boardColorPalette[0].color5}
           </div>
-        </div>
+        </ColorCard>
       </div>
     </div>
   );
--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -1,9 +1,5 @@
 import "./ColorPalette.scss";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
-import copy3 from "../../assets/icons/copy-icon-3.png";
 
 const ColorPalette = ({ colorPalette, onTileClick }) => {
   const ColorCard = styled.div`
@@ -17,24 +13,21 @@ const ColorPalette = ({ colorPalette, onTileClick }) => {
         <div className="palette">
           {colorPalette.map((color) => {
             return (
-              <div className="palette__card">
-                <ColorCard
-                  key={color.color.clean}
-                  style={{
-                    backgroundColor: `${color.color.value}`,
-                  }}
+              <ColorCard
+                key={color.color.clean}
+                style={{
+                  backgroundColor: `${color.color.value}`,
+                }}
+                onClick={() => onTileClick(color.color.value)}
+                className="palette__swatch"
+              >
+                <div
+                  className="palette__info"
                   onClick={() => onTileClick(color.color.value)}
-                  className="palette__swatch"
                 >
-                  <div
-                    className="palette__info"
-                    onClick={() => onTileClick(color.color.value)}
-                  >
-                    <span>{color.color.value}</span>
-                    <img className="palette__copy-img" src={copy3}></img>
-                  </div>
-                </ColorCard>
-              </div>
+                  {color.color.value}
+                </div>
+              </ColorCard>
             );
           })}
         </div>
