7824e5cc23a3421d3f8ec11eaebf4de36a7b7b2a
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-05 12:26:11 -0400

    Added masonry grid styles to images

--- src/components/BoardImages/BoardImages.js
+++ src/components/BoardImages/BoardImages.js
@@ -1,41 +1,39 @@
 import React from "react";
 import "./BoardImages.scss";
+import Masonry from "react-masonry-css";
 
 function BoardImages({ boardImages }) {
+  const breakpointColumnsObj = {
+    default: 5,
+  };
+
   return (
     <div className="board-images">
-      <ul className="board-images__list">
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image1} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image2} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image3} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image4} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image5} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image6} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image7} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image8} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image9} />
-        </li>
-        <li className="board-images__list-item">
-          <img className="board-images__image" src={boardImages[0].image10} />
-        </li>
-      </ul>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="masonry-grid"
+        columnClassName="masonry-grid_column"
+      >
+        <img className="board-images__image" src={boardImages[0].image1} />
+
+        <img className="board-images__image" src={boardImages[0].image2} />
+
+        <img className="board-images__image" src={boardImages[0].image3} />
+
+        <img className="board-images__image" src={boardImages[0].image4} />
+
+        <img className="board-images__image" src={boardImages[0].image5} />
+
+        <img className="board-images__image" src={boardImages[0].image6} />
+
+        <img className="board-images__image" src={boardImages[0].image7} />
+
+        <img className="board-images__image" src={boardImages[0].image8} />
+
+        <img className="board-images__image" src={boardImages[0].image9} />
+
+        <img className="board-images__image" src={boardImages[0].image10} />
+      </Masonry>
     </div>
   );
 }
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Images.scss";
+import Masonry from "react-masonry-css";
 
 function Images({ images }) {
+  const breakpointColumnsObj = {
+    default: 5,
+  };
+
   return (
     <div className="images">
-      <ul className="images__list">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="masonry-grid"
+        columnClassName="masonry-grid_column"
+      >
         {images.map((image) => {
-          return (
-            <li className="images__list-item">
-              <img className="images__image" src={image.src.large} />
-            </li>
-          );
+          return <img className="images__image" src={image.src.large} />;
         })}
-      </ul>
+      </Masonry>
     </div>
   );
 }
