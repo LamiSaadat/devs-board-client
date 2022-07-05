01120754dcb41ff375e2a50ad8c667f0211ddb26
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-05 11:05:40 -0400

    Added image styles

--- src/components/BoardImages/BoardImages.js
+++ src/components/BoardImages/BoardImages.js
@@ -1,18 +1,41 @@
 import React from "react";
+import "./BoardImages.scss";
 
 function BoardImages({ boardImages }) {
   return (
-    <div>
-      <img src={boardImages[0].image1} />
-      <img src={boardImages[0].image2} />
-      <img src={boardImages[0].image3} />
-      <img src={boardImages[0].image4} />
-      <img src={boardImages[0].image5} />
-      <img src={boardImages[0].image6} />
-      <img src={boardImages[0].image7} />
-      <img src={boardImages[0].image8} />
-      <img src={boardImages[0].image9} />
-      <img src={boardImages[0].image10} />
+    <div className="board-images">
+      <ul className="board-images__list">
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image1} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image2} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image3} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image4} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image5} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image6} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image7} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image8} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image9} />
+        </li>
+        <li className="board-images__list-item">
+          <img className="board-images__image" src={boardImages[0].image10} />
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "./Images.scss";
 
 function Images({ images }) {
   return (
-    <div>
-      {images.map((image) => {
-        return <img src={image.src.small} />;
-      })}
+    <div className="images">
+      <ul className="images__list">
+        {images.map((image) => {
+          return (
+            <li className="images__list-item">
+              <img className="images__image" src={image.src.large} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
