a6c99c11a1cf2b8ad0db1df9da42175029ef1e78
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-08-04 17:42:42 -0400

    Add responsive styles to color palette component

--- src/components/ColorPalette/ColorPalette.js
+++ src/components/ColorPalette/ColorPalette.js
@@ -2,31 +2,27 @@ import "./ColorPalette.scss";
 import styled from "styled-components";
 
 const ColorCard = styled.div`
-  width: 8rem;
-  height: 8rem;
+  width: 4rem;
+  height: 4rem;
 `;
 
 const ColorPalette = ({ colorPalette }) => {
   return (
-    <>
-      <div>
-        <div className="palette">
-          {colorPalette.map((color) => {
-            return (
-              <ColorCard
-                key={color.color.clean}
-                style={{
-                  backgroundColor: `${color.color.value}`,
-                }}
-                className="palette__swatch"
-              >
-                <div className="palette__info">{color.color.value} </div>
-              </ColorCard>
-            );
-          })}
-        </div>
-      </div>
-    </>
+    <div className="palette">
+      {colorPalette.map((color) => {
+        return (
+          <ColorCard
+            key={color.color.clean}
+            style={{
+              backgroundColor: `${color.color.value}`,
+            }}
+            className="palette__swatch"
+          >
+            <div className="palette__info">{color.color.value} </div>
+          </ColorCard>
+        );
+      })}
+    </div>
   );
 };
 
